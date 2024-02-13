8725008f89a38f7cc2b0e36a8031060fc3b2af97
Dmitry Shiltsov <sda@asperito.ru>
2024-02-13 19:22:18 +0300

    added poetry & pre-commit

--- main/config_reader.py
+++ main/config_reader.py
@@ -4,6 +4,9 @@ from pydantic import SecretStr
 
 class Settings(BaseSettings):
     bot_token: SecretStr
-    model_config = SettingsConfigDict(env_file='../.env', env_file_encoding='utf-8')
+    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
 
 config = Settings()
+
+
+
